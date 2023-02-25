086b1e70bb807c4501f7455c4fb5fc7e6368da39
Viachaslau <viachaslauihnatsiuk@gmail.com>
2023-02-25 12:50:58 +0300

    refactor: refactor item comment schema

--- dist/models/itemModel.js
+++ dist/models/itemModel.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = require("mongoose");
-const commentSchema = new mongoose_1.Schema({ user: String, text: String }, { timestamps: true });
+const commentSchema = new mongoose_1.Schema({ user: String, text: String, createdAt: Number });
 const itemSchema = new mongoose_1.Schema({
     title: {
         type: String,
--- src/models/itemModel.ts
+++ src/models/itemModel.ts
@@ -1,6 +1,6 @@
 import { Schema, model } from 'mongoose';
 
-const commentSchema = new Schema({ user: String, text: String }, { timestamps: true });
+const commentSchema = new Schema({ user: String, text: String, createdAt: Number });
 
 const itemSchema = new Schema(
   {
